7a918ac48b7790296a29b682ded45d7f9ff66539
MMijoc <markomijoc18@gmail.com>
2020-12-05 13:52:54 +0100

    Updated Event Class, added SelectMenu and AddEvent

--- EventManager/EventManager/Event.cs
+++ EventManager/EventManager/Event.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 
-//		Name, EventType, StartTime i EndTime.
 namespace EventManager
 {
 	enum EventType
@@ -12,12 +11,107 @@ namespace EventManager
 		Concert,
 		StudySession
 	}
-	class Event
+	public class Event
 	{
 		public string Name { get; set; }
-		public int EventType { get; set; }
+		public int Type { get; set; }
 		public DateTime StartTime { get; set; }
-		public DateTime DateTime { get; set; }
+		public DateTime EndTime { get; set; }
+
+		public void InputEventType()
+		{
+			int eventTypeNumber;
+
+			Console.WriteLine("Unesi vrstu eventa\n\t0 - Coffee\n\t1 - Lecture\n\t2 - Concert\n\t3 - StudySession");
+			while (true) {
+				Console.Write("Izbor: ");
+				bool isNumber = int.TryParse(Console.ReadLine(), out eventTypeNumber);
+
+				if (isNumber && eventTypeNumber >= 0 && eventTypeNumber <= 3)
+					break;
+				else
+					Console.WriteLine("Nepravilan unos!");
+			}
+
+			switch (eventTypeNumber)
+			{
+				case 0:
+					Type = (int)EventType.Coffee;
+					break;
+				case 1:
+					Type = (int)EventType.Lecture;
+					break;
+				case 2:
+					Type = (int)EventType.Concert;
+					break;
+				case 3:
+					Type = (int)EventType.StudySession;
+					break;
+				default:
+					Console.WriteLine("Nepravilan unos!");
+					break;
+			}
+
+			return;
+		}
+
+		public void InputStarTime()
+		{
+			DateTime userDateTime;
+
+			Console.Clear();
+			while (true)
+			{
+				Console.WriteLine("Unesite datum i vrijeme početka\n\t(unijeti u formatu: dan mjesec godina sati:minute)");
+				Console.Write("Unos: ");
+
+				if (DateTime.TryParse(Console.ReadLine(), out userDateTime))
+				{
+					Console.WriteLine("Unijeli ste datum {0} {1}, potvrdite unos", userDateTime.ToShortDateString(), userDateTime.ToShortTimeString());
+					if (Program.ConfirmAction())
+						break;
+					
+				}
+				else
+				{
+					Console.WriteLine("Nepravilan unos!");
+				}
+			}
+
+			StartTime = userDateTime;
+		}
+
+		public void InputEndTime()
+		{
+			DateTime userDateTime;
+
+			Console.Clear();
+			while (true)
+			{
+				Console.WriteLine("Unesite datum i vrijeme kraja\n\t(unijeti u formatu: dan mjesec godina sati:minute)");
+				Console.Write("Unos: ");
+
+				if (DateTime.TryParse(Console.ReadLine(), out userDateTime))
+				{
+					if (EndTime < userDateTime) {
+						Console.WriteLine("Vrijeme kraja ne može biti prije vremena početka!");
+						continue;
+					}
+
+					Console.WriteLine("Unijeli ste datum {0} {1}, potvrdite unos", userDateTime.ToShortDateString(), userDateTime.ToShortTimeString());
+					if (Program.ConfirmAction())
+						break;
+
+				}
+				else
+				{
+					Console.WriteLine("Nepravilan unos!");
+				}
+			}
+
+			EndTime = userDateTime;
+		}
 
 	}
+
 }
--- EventManager/EventManager/Program.cs
+++ EventManager/EventManager/Program.cs
@@ -13,6 +13,7 @@ namespace EventManager
 	{
 		static void Main(string[] args)
 		{
+			SelectMenu();
 
 
 
@@ -27,6 +28,7 @@ namespace EventManager
 			Console.WriteLine("5 - Ukloni osobu sa eventa");
 			Console.WriteLine("6 - Ispis detalja eventa");
 			Console.WriteLine("0 - izlaz");
+			Console.Write("Izbor: ");
 
 			return;
 		}
@@ -41,6 +43,101 @@ namespace EventManager
 
 			return;
 		}
+
+		static void SelectMenu()
+		{
+			var EventAndAttendatns = new Dictionary<Event, List<Person>>();
+
+			while (true)
+			{
+				var input = "";
+
+				PrintMainMenu();
+				var isNumber = int.TryParse(input = Console.ReadLine(), out var select);
+
+				if (!isNumber)
+				{
+					Console.WriteLine("Nepoznata naredba \"{0}\"\nPritnisni bilo koju tipku za nastavak . . .", input);
+					Console.ReadLine();
+					Console.Clear();
+					continue;
+				}
+
+				switch (select)
+				{
+					case 1:
+						AddEvent(EventAndAttendatns);
+						break;
+					case 2:
+
+						break;
+					case 3:
+
+						break;
+					case 4:
+
+						break;
+					case 5:
+
+						break;
+					case 6:
+						Console.Clear();
+						PrintSubmenu();
+						break;
+					case 0:
+						return;
+					default:
+						Console.WriteLine("Nepoznata naredba \"{0}\"", input);
+						break;
+				}
+
+				Console.WriteLine("\nPritnisni bilo koju tipku za nastavak . . .");
+				Console.ReadLine();
+				Console.Clear();
+			}
+
+			static int AddEvent(Dictionary<Event, List<Person>> EventAndAttendatns)
+			{
+				var newEvent = new Event();
+				Console.Write("Unesite ime novog eventa: ");
+				////provjerit je li event s imenom vec postoji
+				var name = Console.ReadLine();
+				newEvent.Name = name;
+				newEvent.InputEventType();
+				newEvent.InputStarTime();
+				newEvent.InputEndTime();
+
+				//var attendantsList = new List<Person>();
+
+
+				//EventAndAttendatns.Add(newEvent, attendantsList);
+
+
+
+
+
+
+
+				return (int)ReturnStatus.Success;
+			}
+
+
+
+
+
+
+
+		}
+
+		public static bool ConfirmAction()
+		{
+			Console.WriteLine("Jeste li sigurni da zelite nastaviti?\n\tDA - da želim nastaviti\n\tNE - ne zelim nastaviti (unesi bilo koju tipku za prekid)");
+			Console.Write("Izbor: ");
+			var input = Console.ReadLine();
+
+			return input.Equals("da", StringComparison.OrdinalIgnoreCase);
+		}
+
 
 
 
